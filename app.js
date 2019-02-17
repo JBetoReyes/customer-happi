a2aefacde2902658c221b99e1fb913a7e34455ff
Jose Reyes <josereyes55@hotmail.com>
2019-02-17 13:10:48 -0600

    adds the dotenv module, a hapi server is implemented

---
+++ app.js
@@ -0,0 +1,23 @@
+const Hapi = require('hapi');
+const server = new Hapi.server({
+    host: process.env.SERVER_HOST,
+    port: process.env.SERVER_PORT
+});
+
+server.route({
+    method: 'GET',
+    path: '/customers/{id}',
+    handler: (request, h) => {
+        return `hello world ${server.params.id}`;
+    }
+});
+
+try {
+    const init = async () => {
+        await server.start();
+	console.log(`Running server on port: ${server.info.port}`);    
+    }
+    init();
+} catch(err) {
+    console.log(err.message);
+}
