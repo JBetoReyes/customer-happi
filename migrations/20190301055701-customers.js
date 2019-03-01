cc0b7d0e3d1a0a98b2b03e9704841a20b2c70687
Jose Reyes <josereyes55@hotmail.com>
2019-03-01 00:16:42 -0600

    adds migrations

---
+++ migrations/20190301055701-customers.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.createTable('customer', {
+    id: {
+      type: 'int',
+      primaryKey: true
+    },
+    first_name: {
+      type: 'string',
+      length: 40
+    },
+    last_name: {
+      type: 'string',
+      length: 40
+    },
+    dob: {
+      type: 'date'
+    },
+    email: {
+      type: 'string',
+      length: 50
+    },
+  }, function(err) {
+    if (err) return callback(err);
+    return callback();
+  });
+};
+
+exports.down = function(db) {
+  db.dropTable('user', callback);
+};
+
+exports._meta = {
+  "version": 1
+};
