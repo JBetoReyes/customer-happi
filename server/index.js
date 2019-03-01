a69b6728ce8ae75ceb6f4971dd58ca057396bfc5
Jose Reyes <josereyes55@hotmail.com>
2019-02-28 23:24:33 -0600

    adds a server connection

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const Hapi = require('hapi');
 const decorator = require('./decorators/index');
 const { ServiceFactory } = require('../lib/services');
 const Blipp = require('blipp');
+const db = require('../lib/db');
 
 module.exports = class Server {
 
@@ -19,8 +20,10 @@ module.exports = class Server {
     }
 
     async register() {
-        const services = await ServiceFactory.buildPlugins();        
+        const services = await ServiceFactory.buildPlugins();
+        const dbPlugin = db.buildDbPlugin();
         await this.server.register([
+            dbPlugin,
             ...services,
             {
                 plugin: Blipp
