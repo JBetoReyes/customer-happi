9be0c142965997cb0dab678a6f47149804aad29b
JBetoReyes <josereyes55@hotmail.com>
2019-03-26 23:22:25 -0600

    adds the customers route

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ const server = hapi.server({
     host: process.env.HAPI_HOST
 });
 
+server.route({
+    method: 'GET',
+    path: '/customers',
+    handler: (request, h) => {
+        return JSON.stringify({"data" : [
+            {
+                "id": 1,
+                "firstName": 'Jon',
+                "lastName": 'Doe',
+                "address": "some direction",
+                "phone": "123-123-1234"
+            }
+        ]});
+    }
+});
+
 const init = async () => {
     await server.start();
     console.log(`server started at port: ${ server.info.port }`);
