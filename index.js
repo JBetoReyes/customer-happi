f8e568c44a9f858526daf6d9342e0d72b76f9c2a
JBetoReyes <josereyes55@hotmail.com>
2019-06-09 16:45:20 -0500

    creates a server class and move all the logic to there

--- index.js
+++ index.js
@@ -1,51 +1,18 @@
-const Hapi = require('hapi');
-const debug = require('debug')('app:index');
+const errLog = require('debug')('app:index:error');
+const Server = require('./lib/Server');
 
 const {
-    APP_PORT,
-    APP_HOST
+    APP_PORT: port,
+    APP_HOST: host
 } = process.env;
 
-const server = Hapi.server({
-    port: APP_PORT,
-    host: APP_HOST
+const server = new Server({
+    port,
+    host
 });
 
-server.route({
-    method: 'GET',
-    path: '/customers',
-    handler: (request, h) => {
-        return {
-            "data": [
-                {
-                    "id": "3880f788-4c03-4fd6-889f-95c09983710a",
-                    "firstName": "jon",
-                    "lastName": "doe",
-                    "phone": "132-123-1134",
-                    "email": "myEmail@email.com"
-                },
-                {
-                    "id": "3a8fbfc8-525a-49d9-9286-a8e4caf4f4a7",
-                    "firstName": "jon",
-                    "lastName": "doe",
-                    "phone": "132-123-1134",
-                    "email": "myEmail@email.com"
-                },
-                {
-                    "id": "c07de8fb-c873-421f-a383-5fb4f6bae479",
-                    "firstName": "Jesus",
-                    "lastName": "Juan",
-                    "phone": "132-123-1134",
-                    "email": "myEmail@email.com"
-                }
-            ]
-        }
-    }
-});
-
-const init = async () => {
-    await server.start();
-    debug(`Server listening on host: ${server.info.host} port: ${server.info.port}`);
-};
-
-init();
+try {
+    server.start();
+} catch(err) {
+    errLog(err)
+}
